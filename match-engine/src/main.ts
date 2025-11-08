ba295685e190de07e8abac010e8060f9f37afc65
Your Name <mariaquaresma2003@gmail.com>
2025-11-08 17:58:17 -0300

    fiz match-service e o grpc

--- match-engine/src/main.ts
+++ match-engine/src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
+    transport: Transport.GRPC,
+    options: {
+      package: 'match_engine',
+      protoPath: join(__dirname, 'grpc/match_engine.proto'),
+      url: 'localhost:50053',
+    },
+  });
+
+  await app.listen();
+  console.log('Match-engine gRPC rodando em localhost:50053');
 }
+
 bootstrap();
+
