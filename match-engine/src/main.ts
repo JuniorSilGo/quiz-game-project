ec0e63c4de5e8d86b1fa5531498e2d08e033f5d9
Your Name <mariaquaresma2003@gmail.com>
2025-11-08 18:23:42 -0300

    fiz dois caminhos do proto

--- match-engine/src/main.ts
+++ match-engine/src/main.ts
@@ -2,20 +2,36 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { join } from 'path';
+import { existsSync } from 'fs';
+
+// Função para detectar o caminho correto do .proto tanto npm run start:dev e npm run start:grpc roda sem erros
+function getProtoPath(): string {
+  const distPath = join(__dirname, 'grpc/match_engine.proto');
+  if (existsSync(distPath)) return distPath;
+
+  const tsPath = join(process.cwd(), 'src/grpc/match_engine.proto');
+  if (existsSync(tsPath)) return tsPath;
+
+  throw new Error('Arquivo match_engine.proto não encontrado');
+}
 
 async function bootstrap() {
-  const app = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
-    transport: Transport.GRPC,
-    options: {
-      package: 'match_engine',
-      protoPath: join(__dirname, 'grpc/match_engine.proto'),
-      url: 'localhost:50053',
+  const protoPath = getProtoPath();
+
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.GRPC,
+      options: {
+        package: 'match_engine', 
+        protoPath: protoPath,
+        url: 'localhost:50053',
+      },
     },
-  });
+  );
 
   await app.listen();
   console.log('Match-engine gRPC rodando em localhost:50053');
 }
 
 bootstrap();
-
