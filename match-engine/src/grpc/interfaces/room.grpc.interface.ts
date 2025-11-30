d497f6383a0743bdc3a0b7ecaf34dd06a98f2510
Gabriel Lima <gabrielttnik@gmail.com>
2025-11-30 15:12:49 -0300

    Excluindo match-engine antiga

--- match-engine/src/grpc/interfaces/room.grpc.interface.ts
+++
@@ -1,4 +0,0 @@
-export interface RoomServiceClient {
-  getRoom(data: { roomId: number }): Promise<{ id: number; players: Array<{ id:number; username:string }>; }>;
-  notifyMatchStarted(data: { roomId: number; matchId: number }): Promise<any>;
-}
