5b9e78e23303ce32b2aae7d69aec969d95e107fd
Bianca1232023 <150062646+Bianca1232023@users.noreply.github.com>
2025-12-14 01:30:42 -0300

    feat: adicionar variáveis de ambiente e configurações para microserviços

--- match-service/src/config/grpc.config.ts
+++ match-service/src/config/grpc.config.ts
@@ -4,7 +4,7 @@ import { GrpcOptions, Transport } from '@nestjs/microservices';
 export const grpcServerOptions: GrpcOptions = {
   transport: Transport.GRPC,
   options: {
-    url: '0.0.0.0:50051',
+    url: '0.0.0.0:' + (process.env.GRPC_PORT || '50053'),
     package: 'match',
     protoPath: join(__dirname, '../infrastructure/grpc/proto/match.proto'),
   },
--- room-service/src/main.ts
+++ room-service/src/main.ts
@@ -4,44 +4,25 @@ import { Transport } from '@nestjs/microservices';
 import { join } from 'path';
 
 async function bootstrap() {
-  // Esse aqui é o que vai ficar depois que tudo estiver pronto.
-
-  //   const app = await NestFactory.createMicroservice(AppModule, {
-  //     transport: Transport.GRPC,
-  //     options: {
-  //       package: 'room',
-  //       protoPath: join(process.cwd(), 'proto/room.proto'),
-  //       url: '0.0.0:50051',
-  //       loader: {
-  //         keepCase: true,
-  //       },
-  //     },
-  //   });
-
-  //   await app.listen();
-  //   console.log(
-  //     `Room-service running: gRPC at ${process.env.ROOM_GRPC_URL ?? '0.0.0.0:50051'}`,
-  //   );
-  // }
-
   // 🟦 Cria aplicação HTTP
   const app = await NestFactory.create(AppModule, { cors: true });
 
-  // 🟥 Conecta o microserviço gRPC
+  // 🟥 Conecta o microserviço gRPC na porta 50052
   app.connectMicroservice({
     transport: Transport.GRPC,
     options: {
       package: 'room',
       protoPath: join(process.cwd(), 'proto/room.proto'),
-      url: '0.0.0.0:50051',
+      url: process.env.ROOM_GRPC_URL ?? '0.0.0.0:50052',
       loader: { keepCase: true },
     },
   });
 
   await app.startAllMicroservices();
-  await app.listen(3000);
+  const port = process.env.PORT ?? 3000;
+  await app.listen(port);
 
-  console.log('HTTP rodando em http://localhost:3000');
-  console.log('gRPC rodando em 0.0.0.0:50051');
+  console.log(`HTTP rodando em http://localhost:${port}`);
+  console.log(`gRPC rodando em ${process.env.ROOM_GRPC_URL ?? '0.0.0.0:50052'}`);
 }
 void bootstrap();
